ee231c8e493423b1cb9524613729456564ee1e73
Chansu Park <chance.park@samsung.com>
2025-09-23 14:22:30 +0900

    os/kernel/debug: Move and Rename dead thread stackinfo config
    The option `CONFIG_STACKMONITOR_DISABLE_DEAD_THREAD` was introduced in PR #6945,
    but its placement under the Stackmonitor section and its name do not reflect its real purpose.
    
    This commit includes:
    * Move the option from the Stackmonitor section to the Debug Features section.
    * Remove dependency to Stackmonitor, and Make saving dead‑thread stack information to a generic kernel debug feature, not a Stackmonitor specific one.
    * Rename it to `CONFIG_DISABLE_SAVING_STACKINFO`.

--- apps/system/utils/utils_stackmonitor.c
+++ apps/system/utils/utils_stackmonitor.c
@@ -259,7 +259,6 @@ static void *stackmonitor_daemon(void *arg)
 		printf("|------------");
 #endif
 		printf("\n");
-#if !defined(CONFIG_STACKMONITOR_DISABLE_DEAD_THREAD)
 		stkmon_print_inactive_list();
 		printf("------|----------|----------");
 #ifdef CONFIG_STACK_COLORATION
@@ -273,7 +272,6 @@ static void *stackmonitor_daemon(void *arg)
 		printf("|------------");
 #endif
 		printf("\n");
-#endif //!CONFIG_STACKMONITOR_DISABLE_DEAD_THREAD
 		utils_proc_pid_foreach(stkmon_read_proc, NULL);
 #ifndef CONFIG_DISABLE_SIGNALS
 		sleep(CONFIG_STACKMONITOR_INTERVAL);
