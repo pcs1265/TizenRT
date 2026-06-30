3c3f17d555663e92acd9d0d1f59cacf883feec4d
Chansu Park <chance.park@samsung.com>
2026-06-30 21:18:09 +0900

    qemu-virt: add virtio serial bulk char device

---
+++ os/arch/arm/src/qemu-virt/virtio/virtio-serial.h
@@ -0,0 +1,66 @@
+/****************************************************************************
+ * drivers/virtio/virtio-serial.h
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __DRIVERS_VIRTIO_VIRTIO_SERIAL_H
+#define __DRIVERS_VIRTIO_VIRTIO_SERIAL_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <tinyara/config.h>
+
+#include <stdint.h>
+
+#ifdef CONFIG_QEMU_VIRT_VIRTIO_SERIAL
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+#ifdef __cplusplus
+#define EXTERN extern "C"
+extern "C"
+{
+#else
+#define EXTERN extern
+#endif
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+int virtio_register_serial_driver(void);
+int virtio_serial_driver_initialize(uint32_t device_num);
+
+#undef EXTERN
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CONFIG_QEMU_VIRT_VIRTIO_SERIAL */
+
+#endif /* __DRIVERS_VIRTIO_VIRTIO_SERIAL_H */
